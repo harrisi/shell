2a7bda2fbe43efcc28186151363b1d4ff91f4a9b
Sid Spry <R030t1@gmail.com>
2017-04-24 21:24:17 -0500

    Finish POSIX implementation of environment class.

--- shell.cc
+++ shell.cc
@@ -33,10 +33,17 @@ public:
 
 	const char *const *begin() const { return argv_; }
 	const char *const *end() const { return argv_ + argc_; }
+
 private:
 	const int argc_;
 	const char *const *argv_;
 };
+
+#if defined(POSIX)
+typedef char ** environment_t;
+#elif defined(WINAPI)
+typedef char *  environment_t;
+#endif
 
 // TODO: Read in from OS format, modify, then pack into OS format for process
 // creation.
@@ -44,18 +51,58 @@ private:
 // terminated array of strings.
 class environment {
 public:
-	// TODO: Should default constructor retrieve environment from OS?
 	environment()
+	{ this->from_environment(); }
+	environment(const map<string, string> &vars)
+		: vars_(vars)
 	{ }
 
+	void from_environment();
+	environment_t to_environment();
 	string &operator [] (const string &key);
+
 private:
-	map<string, string> vars;
+	map<string, string> vars_;
 };
 
-string &environment::operator [] (const string &key)
+void
+environment::from_environment()
 {
-	return vars[key];
+#if defined(POSIX)
+	const char *eq;
+	string key, val;
+
+	for (char *const *penv = environ; *penv; penv++) {
+		eq  = strchr(*penv, '=');
+		key = string(*penv, eq - *penv);
+		val = string(eq + 1);
+
+		vars_[key] = val;
+	}
+#elif defined(WINAPI)
+#endif
+}
+
+environment_t
+environment::to_environment()
+{
+#if defined(POSIX)
+	char **env = new char *[vars_.size()],
+		 **penv = env;
+
+	for (const auto &v: vars_) {
+		*penv = strdup((v.first + "=" + v.second).c_str());
+		penv++;
+	}
+	return env;
+#elif defined(WINAPI)
+#endif
+}
+
+string &
+environment::operator [] (const string &key)
+{
+	return vars_[key];
 }
 
 // TODO: Find a good way to handle the difference in environment variable
@@ -66,27 +113,6 @@ string &environment::operator [] (const string &key)
 // TODO: Determine storage requirements of environment.
 // TODO: Determine a possible replacement for environment variables.
 // TODO: Pass `environ` in explicitly?
-#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
-char *const *
-clone_environ()
-{
-	char **env;
-	int nenv = 0;
-
-	for (char *const *penv = environ; *penv; penv++)
-		nenv++;
-
-	env = new char *[nenv + 1];
-	memset(env, 0, nenv + 1);
-
-	// TODO: Use safe string functions. Possibly avoid library functions.
-	for (int ivar = 0; ivar < nenv; ivar++)
-		env[ivar] = strdup(environ[ivar]);
-
-	return env;
-}
-#endif
-
 #if defined(_WIN32)
 const char *
 clone_environ()
@@ -118,19 +144,6 @@ clone_environ()
 }
 #endif
 
-#if defined(__unix__) || (defined(__APPLE__) && defined(__MACH__))
-void
-destroy_environ(char *const *env)
-{
-	int nenv = 0;
-	for (char * const *penv = env; *penv; penv++) {
-		free(*penv);
-		nenv++;
-	}
-	delete[] env;
-}
-#endif
-
 #if defined(_WIN32)
 void
 destroy_environ(char const *env)
@@ -146,18 +159,20 @@ destroy_environ(char const *env)
 int
 spawn(const string &path, const string &args)
 {
-	pid_t child = fork();
+	pid_t child;
+	environment env;
 
 	// TODO: Make caller fill this in.
 	char *const argv[] = {(char *)"", nullptr};
 	// Is this freed properly?
-	char *const *envp  = clone_environ();
+	char *const *envp  = env.to_environment();
 
+	child = vfork();
 	if (child == -1) {
 		return -1;
 	}
 	else if (child) {
-		destroy_environ(envp);	
+		//destroy_environ(envp);	
 		return child;
 	} 
 	else {
@@ -190,14 +205,7 @@ int
 main(int argc, char *argv[]) 
 {
 	int ls = spawn("/bin/ls", "");
-	int status;
-
-	//const char *env = clone_environ();
-	//destroy_environ(env);
-	
-	if (join(ls, &status) == -1)
-		// TODO: Use strerror_r.
-		cout << "join: " << strerror(errno) << std::endl;
+	(void)ls;
 
 	return 0;
 }
