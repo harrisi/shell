0cf152fa92bbc554296ea029bd029d507f77a56a
Sid Spry <R030t1@gmail.com>
2017-04-27 21:41:34 -0500

    Add UTF-8 validation.

--- shell.c
+++ shell.c
@@ -13,16 +13,6 @@ struct buffer
 	uint8_t *data;
 };
 
-struct buffer *
-buffer_alloc(size_t size)
-{
-	struct buffer *b = malloc(sizeof(struct buffer));
-	if (!b)
-		return b;
-	b->data = malloc(sizeof(uint8_t) * size);
-	return b;
-}
-
 void
 buffer_free(struct buffer *b)
 {
@@ -30,15 +20,68 @@ buffer_free(struct buffer *b)
 	free(b);
 }
 
+struct buffer *
+buffer_alloc(size_t size)
+{
+	struct buffer *b = malloc(sizeof(struct buffer));
+	if (!b)	return NULL;
+	if (!(b->data = malloc(sizeof(uint8_t) * size))) {
+		free(b);
+		return NULL;
+	}
+	b->size = size;
+	return b;
+}
+
+uint8_t *
+utf_validate(uint8_t *data)
+{
+	while (*data) {
+		if (*data < 0x80) data++;
+		else if ((data[0] & 0xE0) == 0xC0) {
+			if ((data[1] & 0xC0) != 0x80 ||
+				(data[0] & 0xFE) == 0xC)
+				return data;
+			else data += 2;
+		}
+		else if ((data[0] & 0xF0) == 0xE0) {
+			if ((data[1] & 0xC0) != 0x80 ||
+				(data[2] & 0xC0) != 0x80 ||
+				(data[0] == 0xE0 && (data[1] & 0xE0) == 0x80) ||
+				(data[0] == 0xED && (data[1] & 0xE0) == 0xA0) ||
+				(data[0] == 0xEF && data[1] == 0xBF &&
+				 (data[2] & 0xFE) == 0xBE))
+				return data;
+			else data += 3;
+		}
+		else if ((data[0] & 0xF8) == 0xF0) {
+			if ((data[1] & 0xC0) != 0x80 ||
+				(data[2] & 0xC0) != 0x80 ||
+				(data[3] & 0xC0) != 0x80 ||
+				(data[0] == 0xF0 && (data[1] & 0xF0) == 0x80) ||
+				(data[0] == 0xF4 && data[1] > 0x8F) || data[0] > 0xF4)
+				return data;
+			else data += 4;
+		}
+		else return data;
+	}
+	return NULL;
+}
+
+uint8_t *
+utf_next(uint8_t *data)
+{
+	return NULL;
+}
+
 // TODO: Specify rules for `indent`.
 // TODO: Implement process spawning with clone().
 int
 main(int argc, char *argv[])
 {
-	struct buffer *b = buffer_alloc(13);
-	memcpy(b->data, "Hello, world!", 13);
-	printf("%s\n", b->data);
-	buffer_free(b);
-
+	if (!utf_validate((uint8_t *)""))
+		printf("Invalid.\n");
+	else
+		printf("Valid.\n");
 	return 0;
 }
